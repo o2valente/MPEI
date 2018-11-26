9ea66d39d74c2f6d079cfac491b9a5375ae749a0
Pedro Almeida <44822725+pedroalmeida22@users.noreply.github.com>
2018-11-26 17:42:50 +0100

    Add files via upload

---
+++ mpei/BloomFilter.java
@@ -0,0 +1,25 @@
+package mpei;
+
+public class BloomFilter {
+	private int k;			// number of hash functions to be used ((log2 * n)/m) -> valor otimo para k
+	private int n;          // number of elements in the filter
+	private int m;			// number of set members
+	 
+	public BloomFilter(int k, int n, int m) {
+		
+	}
+	
+	
+	public void insert() {
+		
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public boolean isMember() {
+		
+		return false;
+	}
+}
